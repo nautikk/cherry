e458e783a644aff81f04a08706c682ddc352886c
Nautikk <helicalnau@gmail.com>
2024-05-12 16:50:24 +0800

    first init

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask
+
+import RPi.GPIO as GPIO
+
+app = Flask(__name__)
+
+# Set the GPIO mode
+GPIO.setmode(GPIO.BOARD)
+
+# Define the GPIO pin you want to use
+pin_fan = 11  # GPIO pin connected to the fan
+
+# Setup the fan pin as an output
+GPIO.setup(pin_fan, GPIO.OUT)
+
+@app.route('/turn_on')
+def turn_on():
+    GPIO.output(pin_fan, GPIO.HIGH)  # Turn on the fan
+    return 'Fan turned on'
+
+@app.route('/turn_off')
+def turn_off():
+    GPIO.output(pin_fan, GPIO.LOW)   # Turn off the fan
+    return 'Fan turned off'
+
+if __name__ == '__main__':
+    try:
+        app.run(host='0.0.0.0', port=5000)
+    finally:
+        # Clean up GPIO on exit
+        GPIO.cleanup()
